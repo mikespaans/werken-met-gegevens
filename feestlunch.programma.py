77c108bf0c80db36b4eb7758a1db5d376f55433a
Mike Spaans <99069233@mydavinci.nl>
2021-09-20 13:26:32 +0200

    werken met gegevens

---
+++ feestlunch.programma.py
@@ -0,0 +1,14 @@
+
+croisantjes = float(17)
+croisant = float(0.39) 
+stokbroodd = float(2.78)
+stokbrodenn = float(2)
+kortingsbonnenn = float(3)
+kortingsbedragg = float(1.50)
+eindbedragg = float(10.69)
+bedragg = 12.19
+berekening = (croisantjes * croisant + stokbrodenn * stokbroodd)
+eindbereking = str(bedragg - kortingsbedragg)
+print (str(bedragg - kortingsbedragg))
+print ("de feestlunch kost je bij de bakker " + str(eindbereking) + " euro voor de " +  str(croisantjes) + " croissantjes en de " +  str(stokbrodenn) + " stokbroden als de " +  str(kortingsbonnenn) + " kortingsbonnen nog geldig zijn!")
+
